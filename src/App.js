57f8c66f5c1c8503ab545eff7d4bd98aad6e7a4e
small-Hayoola <pooya@boostanchi.com>
2021-06-01 13:25:20 +0430

    Enabled Routing System

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
 
 function App() {
   return (
     <div>
       <h1>Hello Pastep!</h1>
+      <Router>
+        <Route path='/' exact><h1>HomePage</h1></Route>
+      </Router>
     </div>
   );
 }
