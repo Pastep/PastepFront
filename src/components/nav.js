fda9f81585519d1fc970219ef5cfd0b8d4f3eea7
small-Hayoola <pooya@boostanchi.com>
2021-06-01 14:05:58 +0430

    Fixed avatar size

--- src/components/nav.js
+++ src/components/nav.js
@@ -13,7 +13,7 @@ function Nav() {
             </div>
             <div className='navbar-left'>
                 <a className='nav-button' href="#">پیست جدید</a>
-                <img width='52px' height='52px' src="https://cdn.discordapp.com/avatars/721402598326009988/c63434b7f1ba424088f802f3252a02d5.png?size=1024"></img>
+                <img width='49px' height='49px' src="https://cdn.discordapp.com/avatars/721402598326009988/c63434b7f1ba424088f802f3252a02d5.png?size=1024"></img>
                 <img style={{ transform:'translateY(1px)' }} width="24px" height='24px' src={bell}></img>
                 <div className='nav-search-box'>
                     <img width='20px' height='20px' src={search}></img>
